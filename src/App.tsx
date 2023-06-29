0d2063d7dba44015e3ef118d6ea05700722bb40e
Joost Slijkoort <joost.slijkoort@philips.com>
2023-06-29 15:18:25 +0200

    Add PivotControls

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { PivotControls } from '@react-three/drei'
 import './App.css'
 
 function App() {
@@ -9,11 +9,17 @@ function App() {
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
-        <OrbitControls />
-        <mesh scale={1}>
-          <capsuleGeometry args={[1, 1, 32, 32]} />
-          <meshStandardMaterial color={'hotpink'} />
-        </mesh>
+        <PivotControls
+          scale={2}
+          disableSliders={true}
+          anchor={[0, 0.35, 0]}
+          depthTest={false}
+          >
+            <mesh>
+              <capsuleGeometry args={[1, 1, 32, 32]} />
+              <meshStandardMaterial color={'hotpink'} />
+            </mesh>
+        </PivotControls>
       </Canvas>
     </>
   )
