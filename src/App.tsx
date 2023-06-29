02b42dd71519470a8e5ab1ac7cfd01abdd2fa30e
Joost Slijkoort <joost.slijkoort@philips.com>
2023-06-29 15:05:11 +0200

    Setup basic Three scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 import './App.css'
 
 function App() {
   return (
     <>
-      Hello world!
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <OrbitControls />
+        <mesh scale={1}>
+          <capsuleGeometry args={[1, 1, 32, 32]} />
+          <meshStandardMaterial color={'hotpink'} />
+        </mesh>
+      </Canvas>
     </>
   )
 }
